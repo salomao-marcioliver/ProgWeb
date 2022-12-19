50cc2dc10aa3776457d00a3f5557bcfc293f7113
Marcioliver <salomaomarcioliver@gmail.com>
2022-12-19 20:08:11 -0300

    Version 0.3

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
 import mysql from 'mysql2'
 
+dotenv.config()
+
 export const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Database09",
-    database: "prog-web"
+    host: process.env.HOST,
+    user: process.env.USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
